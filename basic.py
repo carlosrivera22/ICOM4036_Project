12802e3b2cd44220fab9b206e01b9f5fdc45c840
carlosrivera22 <carlos.rivera22@upr.edu>
2017-04-11 18:09:09 -0400

    SPL
    SPL

--- basic.py
+++ basic.py
@@ -1,5 +1,5 @@
 from sys import *
-import os
+
 
 tokens = []
 symbols = {}
@@ -13,8 +13,8 @@ def lexer(filecontents):
     tok=""
     string=""
     state = 0
-    isexpr = 0
-    expr = ""
+    istag = 0
+    tag = ""
     n = ""
     varstarted = 0
     var = ""
@@ -25,13 +25,18 @@ def lexer(filecontents):
             if state == 0:
                 tok = ""
         elif tok == "\n" or tok == "<EOF>": #what will happen if we reach a new line or the end of the file?
-            if var != "":
-                tokens.append("VAR:" + var)
-                var = ""
-                varstarted = 0
             tok = ""
         elif tok == "START" or tok == "start":
             tokens.append("START")
+            tok = ""
+        elif tok == "ADDINSIDE" or tok == "addinside":
+            tokens.append("ADDINSIDE")
+            tok = ""
+        elif tok == "ADDBEFORE" or tok == "addbefore":
+            tokens.append("ADDBEFORE")
+            tok = ""
+        elif tok == "ADDAFTER" or tok == "addafter":
+            tokens.append("ADDAFTER")
             tok = ""
         elif tok == "\"" or tok == " \"":
             if state == 0:
@@ -48,17 +53,65 @@ def lexer(filecontents):
     print(tokens)
     return tokens
 
+
+
 def create_file(filename):
     filename = filename[8:-1] + ".html"
-    open(filename,"w")
+    file = open(filename,"w+")
+    file.write("<!DOCTYPE html>\n<html>\n<title></title>\n<body>\n</body>\n</html>")
+
+def get_file_contents(filename):
+    f = open(filename,"r")
+    contents = f.readlines()
+    f.close()
+    return contents
+
+def write_in_file(filename, contents):
+    f = open(filename,"w")
+    contents = "".join(contents)
+    f.write(contents)
+    f.close()
+
+#make a method to get the index of where the tag will be placed
+
+#remember that the tag will be placed after,before or inside another tag...
+#maybe change the create command to addinside, addafter and addbefore commands and deal with them separately
+
+def add_inside(target,tag,id):
+    if tag == "p" or tag == "h1" or tag == "h2" or tag == "h3" or tag == "h4" or tag == "h5" or tag == "h6":
+        print("valid tag")
+        contents = get_file_contents("index.html")
+        index = find_index_inside(target)
+        tag = "<" + tag + " id='"+ id + "'>\n" + "</" + tag + ">\n"
+        contents.insert(index,tag)
+        write_in_file("index.html",contents)
+        print(contents)
+    else:
+        print("not a valid tag") #throw an error
+
+def find_index_inside(target):
+    contents = get_file_contents("index.html")
+    target = "<" + target + ">\n"
+    print(target)
+    i = 0
+    while(i < len(contents)):
+        if contents[i] == target:
+            return i + 1
+        i+=1
+    return False #throw an error
+
 
 def parser(toks):
     i = 0
     while(i < len(toks)):
         if toks[i] + " " + toks[i+1][0:6] == "START STRING":
             create_file(toks[i+1])
-            i += 2
-        i+=1
+            i+=2
+        elif toks[i] + " " + toks[i+1][0:6] + " " + toks[i+2][0:6] + " " + toks[i+3][0:6] == "ADDINSIDE STRING STRING STRING":
+            add_inside(toks[i+1][8:-1],toks[i+2][8:-1], toks[i+3][8:-1])
+            i+=4
+
+
 
 
 
