adcc77d8a44777e72e94151db0e7970e6e6cdb33
carlosrivera22 <carlos.rivera22@upr.edu>
2017-04-12 23:08:59 -0400

    SPL
    SPL

--- basic.py
+++ basic.py
@@ -8,6 +8,27 @@ def open_file(filename):
     data += "<EOF>"
     return data
 
+
+
+
+
+def create_file(filename):
+    filename = filename[8:-1] + ".html"
+    file = open(filename,"w+")
+    file.write("<!DOCTYPE html>\n<html>\n<title>\n</title>\n<body>\n</body>\n</html>")
+
+def get_file_contents(filename):
+    f = open(filename,"r")
+    contents = f.readlines()
+    f.close()
+    return contents
+
+def write_in_file(filename, contents):
+    f = open(filename,"w")
+    contents = "".join(contents)
+    f.write(contents)
+    f.close()
+
 def lexer(filecontents):
     tok=""
     string=""
@@ -28,6 +49,9 @@ def lexer(filecontents):
         elif tok == "START" or tok == "start":
             tokens.append("START")
             tok = ""
+        elif tok == "PUT" or tok == "put":
+            tokens.append("PUT")
+            tok = ""
         elif tok == "ADDINSIDE" or tok == "addinside":
             tokens.append("ADDINSIDE")
             tok = ""
@@ -36,6 +60,9 @@ def lexer(filecontents):
             tok = ""
         elif tok == "ADDAFTER" or tok == "addafter":
             tokens.append("ADDAFTER")
+            tok = ""
+        elif tok == "ADDCOMPONENT" or tok == "addcomponent":
+            tokens.append("ADDCOMPONENT")
             tok = ""
         elif tok == "ADD " or tok == "add ":
             tokens.append("ADD")
@@ -56,29 +83,6 @@ def lexer(filecontents):
     return tokens
 
 
-
-def create_file(filename):
-    filename = filename[8:-1] + ".html"
-    file = open(filename,"w+")
-    file.write("<!DOCTYPE html>\n<html>\n<title></title>\n<body>\n</body>\n</html>")
-
-def get_file_contents(filename):
-    f = open(filename,"r")
-    contents = f.readlines()
-    f.close()
-    return contents
-
-def write_in_file(filename, contents):
-    f = open(filename,"w")
-    contents = "".join(contents)
-    f.write(contents)
-    f.close()
-
-#make a method to get the index of where the tag will be placed
-
-#remember that the tag will be placed after,before or inside another tag...
-#maybe change the create command to addinside, addafter and addbefore commands and deal with them separately
-
 def add(tag, id):
     if tag == "p" or tag == "h1" or tag == "h2" or tag == "h3" or tag == "h4" or tag == "h5" or tag == "h6" or tag == "div":
         contents = get_file_contents("index.html")
@@ -86,38 +90,45 @@ def add(tag, id):
         tag = "<" + tag + " id='"+ id + "'>\n" + "</" + tag + ">\n"
         contents.insert(index,tag)
         write_in_file("index.html",contents)
-        print(contents)
+
     else:
         print("not valid tag for inside body")
 
 def add_inside(target,tag,id):
     if tag == "p" or tag == "h1" or tag == "h2" or tag == "h3" or tag == "h4" or tag == "h5" or tag == "h6":
-        print("valid tag")
+
         contents = get_file_contents("index.html")
         index = find_index_inside(target)
         tag = "<" + tag + " id='"+ id + "'>\n" + "</" + tag + ">\n"
         contents.insert(index,tag)
         write_in_file("index.html",contents)
-        print(contents)
+
     else:
         print("not a valid tag") #throw an error
 
 def find_index_inside(target):
     contents = get_file_contents("index.html")
-    print(target)
+
     i = 0
     while(i < len(contents)):
         if contents[i].find(target) == -1:
             i+=1
         else:
-            print("found")
+
             return i+1
     return False #throw an error
+
+def put_content(target,content):
+    content = content + "\n"
+    contents = get_file_contents("index.html")
+    index = find_index_inside(target)
+    contents.insert(index,content)
+    write_in_file("index.html",contents)
 
 def find_body_index():
     contents = get_file_contents("index.html")
     target = "<body>\n"
-    print(target)
+
     i = 0
     while(i < len(contents)):
         if contents[i] == target:
@@ -131,18 +142,23 @@ def parser(toks):
         if toks[i] + " " + toks[i+1][0:6] == "START STRING":
             create_file(toks[i+1])
             i+=2
+        elif toks[i] + " " + toks[i+1][0:6] + " " + toks[i+2][0:6] == "PUT STRING STRING":
+            print("PUTTING CONTENT...")
+            put_content(toks[i+1][8:-1],toks[i+2][8:-1])
+            i+=3
         elif toks[i] + " " + toks[i+1][0:6] + " " + toks[i+2][0:6] + " " + toks[i+3][0:6] == "ADDINSIDE STRING STRING STRING":
             add_inside(toks[i+1][8:-1],toks[i+2][8:-1], toks[i+3][8:-1])
             i+=4
         elif toks[i] + " " + toks[i+1][0:6] + " " + toks[i+2][0:6] == "ADD STRING STRING":
             add(toks[i+1][8:-1],toks[i+2][8:-1])
             i+=3
-        if(i == len(toks)):
+        else:
             i+=1
 
 def run():
     data = open_file(argv[1])
     toks = lexer(data)
     parser(toks)
+    print(get_file_contents("index.html"))
 
 run()
